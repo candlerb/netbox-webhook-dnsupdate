9a67c62174d93f07d6a2161b7a8e4d50763852a3
Brian Candler <b.candler@pobox.com>
2021-08-09 17:44:32 +0100

    Simplify logic for Netbox v2.11+
    Thanks to https://github.com/netbox-community/netbox/issues/3451

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ and reverse (PTR).
 
 It has an updater class for RFC2136 dynamic DNS updates, but could be
 extended to access other APIs or databases.  The backend just needs to be
-able to add and remove specific A/AAAA and PTR records, and list existing
-ones.
+able to add and remove specific A/AAAA and PTR records.
 
 It is assumed that Netbox maintains only the "primary" name for each IP
 address - that is, the one which the PTR record refers to.  Additional
@@ -15,6 +14,9 @@ records such as MX or CNAME, or additional A/AAAA records pointing to the
 same IP address, still need to be added outside of Netbox.
 
 ## Dependencies
+
+**This version requires netbox v2.11+ as it uses the prechange/postchange
+feature in webhooks.  An older version is in the `pre-v2.11` branch**
 
 Requires [dnspython](http://www.dnspython.org/examples.html).  Tested with
 version 0.16, which can be installed using pip:
@@ -48,39 +50,14 @@ would have done, uncomment this line:
 
 ## Logic
 
-The update message from Netbox does not [provide the old
-data](https://github.com/netbox-community/netbox/issues/3451).  This makes
-this updater much more complicated that it otherwise might be.
+When an IP address is updated and the address or DNS name is different to
+what it was before:
 
-The webhook performs DNS queries to find out what records already exist, to
-work out whether records needed to be added and/or removed.
-
-When an IP address record with dnsname is saved:
-
-* The name is queried for existing A or AAAA records (depending on the address
-  family of the ipaddress object)
-* Any existing A/AAAA records for this name which point to *another* IP address
-  are removed
-* In addition, any PTR records from that other IP address which point back
-  to the same name are removed
-
-Also:
-
-* The address is queried for existing PTR records
-* Any existing PTR records which point to *another* name are removed
-* In addition, any A/AAAA records from that other name which point to
-  the same IP address are removed
+* If an old name was present, then the A (or AAAA) and PTR records are removed
+* If a new name is present, then new A (or AAAA) and PTR records are created
 
 ## Caveats and limitations
 
-* Any save of an ipaddress object will cause the DNS records to be queried
-  and reconciled, even if the `address` and `dns_name` have not changed
-* **If you save any ipaddress object where the dns_name is empty, this will
-  cause all existing PTR records for that IP address to be removed** (plus,
-  if there was a forward DNS name which is the same as the PTR record, that
-  forward DNS entry will be removed as well)
-* If you change *both* the `address` and `dns_name` of an ipaddress object
-  at the same time, the old name and address will remain in the DNS
 * You have to configure each zone that you want to update.  It does not work
   out for itself where the zone cuts are.
 * Netbox does not have a [unique constraint](https://github.com/netbox-community/netbox/issues/3490)
@@ -89,9 +66,6 @@ Also:
   things can happen if the same IP address exists multiple times, e.g. in
   different VRFs.
 * No SSL support
-* ~~See also [this bug](https://github.com/netbox-community/netbox/issues/3489)
-  which causes an exception when you delete an ipaddress.~~ (Fixed in
-  Netbox v2.6.4)
 
 ## Licence
 
--- nb_dns_updater.py
+++ nb_dns_updater.py
@@ -1,21 +1,12 @@
 """
 This is a dynamic DNS webhook for Netbox.  It updates A/AAAA and PTR records
 based on the dns_name attribute of ipam.ipaddress records.
-
-BEWARE: if you save any IP address object where the dns_name is empty, it will
-remove any existing PTR (reverse DNS records) for that address.
-
-NOTE: if you modify an existing ipaddress record, it will work if you change
-the name *or* the address, but not both at the same time.  This is because of
-a limitation of webhooks where it does not pass the old and new values.
-See https://github.com/netbox-community/netbox/issues/3451
 """
 
 import dns.name
 import dns.update
 import dns.query
 import dns.rcode
-import dns.resolver
 import dns.reversename
 import hashlib
 import hmac
@@ -44,9 +35,7 @@ class DDNSUpdater:
         self.server = socket.getaddrinfo(server, None)[0][4][0]
         self.debug = debug
         self.kwargs = kwargs
-        self.resolver = dns.resolver.Resolver(configure=False)
-        self.resolver.nameservers = [self.server]
-     
+
     def __call__(self, zone, updates):
         """
         Takes a list of updates and sends a DDNS update request
@@ -64,9 +53,6 @@ class DDNSUpdater:
         if rcode != dns.rcode.NOERROR:
             print("DNS update for %s failed: %s" % (zone, dns.rcode.to_text(rcode)),
                   file=sys.stderr)
-    
-    def query(self, *args):
-        return self.resolver.query(*args)
 
 class UpdateMapper:
     """
@@ -75,7 +61,7 @@ class UpdateMapper:
     updates.  At commit time, the list of updates is passed to the relevant updater.
     """
     def __init__(self, zones):
-        self.zones = { dns.name.from_text(n): v for n, v in zones.items() } 
+        self.zones = { dns.name.from_text(n): v for n, v in zones.items() }
 
     def begin(self):
         """Return a mapping of dns.name to updates"""
@@ -90,7 +76,7 @@ class UpdateMapper:
                 dnsname = dnsname.parent()
             except dns.name.NoParent:
                 return None
- 
+
     def _record(self, ctx, action, dnsname, *args):
         """
         Record an update required.  Format is same as calls to dns.update.Update but
@@ -118,18 +104,6 @@ class UpdateMapper:
         for n, updates in ctx.items():
             self.zones[n](n, updates)
 
-    def query(self, dnsname, *args):
-        """
-        Send a query to the authoritative server if possible; fallback to
-        the system recursive resolver
-        """
-        zonename = self._find(dnsname)
-        if zonename:
-            updater = self.zones[zonename]
-            if hasattr(updater, 'query'):
-                return updater.query(dnsname, *args)
-        return dns.resolver.query(dnsname, *args)
-
 class DNSWebHook:
     """
     WSGI application to receive webhook requests for ipaddress objects
@@ -148,7 +122,7 @@ class DNSWebHook:
             status = '405 Method not allowed'
             start_response(status, headers)
             return [b'Method not allowed']
-        
+
         try:
             request_body_size = int(environ.get('CONTENT_LENGTH', 0))
         except (ValueError):
@@ -171,89 +145,56 @@ class DNSWebHook:
             status = '400 Wrong model'
             start_response(status, headers)
             return [b'Wrong model']
-        event = body["event"]
-        data = body["data"]
+        snapshots = body["snapshots"]
+        oldaddress = newaddress = oldname = newname = None
+        pre = snapshots["prechange"]
+        if pre:
+            oldaddress = pre["address"].split("/")[0]
+            oldname = pre["dns_name"]
+        post = snapshots["postchange"]
+        if post:
+            newaddress = post["address"].split("/")[0]
+            newname = post["dns_name"]
 
-        if event in ["created", "updated"]:
-            self.update_dns(data["address"].split("/")[0], data["dns_name"])
-        elif event == "deleted":
-            self.delete_dns(data["address"].split("/")[0], data["dns_name"])
+        self.update_dns(oldaddress, oldname, newaddress, newname)
 
         status = '200 OK'  # HTTP Status
         start_response(status, headers)
         return [b'OK']
 
-    def update_dns(self, address, name):
+    def update_dns(self, oldaddress, oldname, newaddress, newname):
         """
-        Core update logic.  If name is not None, ensure there is an A/AAAA record from
-        name to address, and that there is a PTR record from address to name.
+        Core update logic:
 
-        Also find and remove any stale A/AAAA and PTR records.
+        - Remove existing A/AAAA record from oldname
+          to oldaddress, and PTR record from oldaddress to oldname
+
+        - Add A/AAAA record from newname to newaddress,
+          and PTR record from newaddress to newname
         """
-        revname = dns.reversename.from_address(address)
-        rrname = name and dns.name.from_text(name)   # may be None
-        rrtype = "AAAA" if revname.is_subdomain(IP6_ARPA) else "A"
+        # Short cut: no changes
+        if not oldname and not newname:
+            return  # no DNS both before and after
+        if oldname == newname and oldaddress == newaddress:
+            return  # same DNS both before and after
+
         ctx = self.mapper.begin()
 
-        # If name is given, iterate over its existing A/AAAA records
-        if name:
-            found = False
-            try:
-                rr = self.mapper.query(rrname, rrtype)
-            except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
-                rr = []
-            for r in rr:
-                if r.address == address:
-                    found = True
-                else:
-                    # Remove this record
-                    self.mapper.delete(ctx, rrname, rrtype, r.address)
-                    # In addition: if we just deleted an A/AAAA record, and there
-                    # is a PTR record from that address which points to this same
-                    # hostname, delete that PTR record too
-                    other = dns.reversename.from_address(r.address)
-                    self.mapper.delete(ctx, other, "PTR", rrname.to_text())
-            if not found:
-                self.mapper.add(ctx, rrname, self.ttl, rrtype, address)
+        if oldaddress and oldname:
+            revname = dns.reversename.from_address(oldaddress)
+            rrtype = "AAAA" if revname.is_subdomain(IP6_ARPA) else "A"
+            rrname = dns.name.from_text(oldname)
+            self.mapper.delete(ctx, rrname, rrtype, oldaddress)
+            self.mapper.delete(ctx, revname, "PTR", rrname.to_text())
 
-        # For the address, iterate over its existing PTR records
-        found = False
-        try:
-            rr = self.mapper.query(revname, "PTR")
-        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
-            rr = []
-        for r in rr:
-            if name and r.target == rrname:
-                found = True
-            else:
-                # Remove this record
-                self.mapper.delete(ctx, revname, "PTR", r.target.to_text())
-                # In addition: if we just deleted a PTR record, and there is
-                # an A/AAAA record from that hostname which points to this same
-                # address, delete that A/AAAA record too
-                self.mapper.delete(ctx, r.target, rrtype, address)
-                    
-        if name and not found:
+        if newaddress and newname:
+            revname = dns.reversename.from_address(newaddress)
+            rrtype = "AAAA" if revname.is_subdomain(IP6_ARPA) else "A"
+            rrname = dns.name.from_text(newname)
+            self.mapper.add(ctx, rrname, self.ttl, rrtype, newaddress)
             self.mapper.add(ctx, revname, self.ttl, "PTR", rrname.to_text())
 
-        # Looks good, apply all the changes
         self.mapper.commit(ctx)
-
-    def delete_dns(self, address, name):
-        """
-        Delete is simple: just remove A/AAAA and PTR (if they exist).
-        If an address with no name is deleted, do nothing - this will leave
-        any existing reverse DNS untouched.
-        """
-        if name:
-            revname = dns.reversename.from_address(address)
-            rrname = dns.name.from_text(name)
-            rrtype = "AAAA" if revname.is_subdomain(IP6_ARPA) else "A"
-
-            ctx = self.mapper.begin()
-            self.mapper.delete(ctx, rrname, rrtype, address)
-            self.mapper.delete(ctx, revname, "PTR", rrname.to_text())
-            self.mapper.commit(ctx)
 
 def simple_server(zones, host='', port=7001, **kwargs):
     from wsgiref.simple_server import make_server
@@ -266,11 +207,8 @@ def simple_server(zones, host='', port=7001, **kwargs):
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    # Quick hack for testing from CLI: pass address and name, and
-    # it shows the updates it would make
+    # Quick hack for testing from CLI: pass old address, old name,
+    # new address and new name, and it shows the updates it would make
     mapper = UpdateMapper({"." : DummyUpdater()})
-    hook = DNSWebHook(mapper) 
-    if sys.argv[1] == "--delete":
-        hook.delete_dns(sys.argv[2], sys.argv[3])
-    else:
-        hook.update_dns(sys.argv[1], sys.argv[2])
+    hook = DNSWebHook(mapper)
+    hook.update_dns(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
